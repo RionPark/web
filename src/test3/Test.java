a385e21f309f9c7e0552a293919f0db002292b93
Rion Park <redfilaruntime@gmail.com>
2021-01-07 14:58:46 +0900

    ㅅㅅ

--- src/test3/Test.java
+++ src/test3/Test.java
@@ -1,24 +1,30 @@
 package test3;
 
-class F1{
-	int a=1;
+class F1 {
+	int a = 1;
 }
-public class Test extends F1{
-	
+
+public class Test extends F1 {
+
 	public Test() {
 		this(1);
 		System.out.println("하하");
 	}
+
 	public Test(int i) {
-		this(1+"");
+		this(1 + "");
 		System.out.println("히히");
 	}
+
 	public Test(String i) {
 		System.out.println("히히");
 	}
-	
+
 	public static void main(String[] args) {
-		Test t = new Test();
-//		System.out.println(t.a);
+		int i=1;
+		String str = "1";
+		if(str.equals("1")) {
+			System.out.println("아 str은 1이구나~");
+		}
 	}
 }
